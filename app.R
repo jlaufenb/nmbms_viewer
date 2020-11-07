dab15d5fdd03bce0e682c96b8bd78ab2b417038e
Laufenberg <jared_laufenberg@fws.gov>
2020-11-06 16:45:37 -0900

    Initial commit

---
+++ app.R
@@ -0,0 +1,159 @@
+#' @title Kodiak NWR Nearshore Survey viewer
+#'
+#' @description  An R shiny app for visualizing distance sampling transects.
+#' @author Jared Laufenberg \email{jared_laufenberg@@fws.gov}
+#'
+
+
+# Install the required packages if they aren't already installed
+packages <- c("shiny", "shinyjs", "tidyverse", "leaflet", "leaflet.minicharts", "shinyWidgets", "DT", "sp",
+              "rgdal","adehabitatLT", "adehabitatHR", "lubridate", "geojsonio",
+              "maptools", "dplyr")
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+    install.packages(setdiff(packages, rownames(installed.packages())))
+}
+
+# Load the required packages
+library(shiny)
+library(shinyjs)
+library(tidyverse)
+library(leaflet)
+library(leaflet.minicharts)
+library(shinyWidgets)
+library(DT)
+library(sp)
+library(rgdal)
+library(adehabitatLT)
+library(adehabitatHR)
+library(lubridate)
+library(geojsonio)
+library(maptools)
+library(dplyr)
+
+# Source custom functions
+source("code/global.R")
+
+# Define UI for application that draws a histogram
+ui <- navbarPage("Transect-Viewer v.0.0.9000", id="nav",
+
+                 # Tab 1: Load data
+                 tabPanel("Load data",
+                          # Sidebar layout
+                          fluidPage(
+                              fileInput(inputId = "file",
+                                        label = "Select transect data:",
+                                        multiple = FALSE,
+                                        accept = ".RData")), # Get the file upload control option
+
+                          DT::DTOutput("sum.tbl")
+                 ),
+
+                 # Tab 2: Interactive map
+                 tabPanel("Interactive map",
+                          div(class = "outer",
+
+                              tags$head(includeCSS("css/style.css")),  # Add custom css style
+
+                              leafletOutput("map", width="100%", height="100%"),  # Add the leaflet map
+
+                              absolutePanel(id = "controls",
+                                            class = "panel panel-default",
+                                            fixed = TRUE,
+                                            draggable = TRUE,
+                                            top = 60,
+                                            left = 20,
+                                            right = "auto",
+                                            bottom = "auto",
+                                            width = "20%",
+                                            height = "auto",
+
+                                            htmlOutput("dataInfo"),  # Add a header of summaries of # of fixes, first/last fix, etc.
+
+                                            shinyWidgets::selectizeGroupUI(  # Add the data filters
+                                                id = "my-filters",
+                                                inline = FALSE,
+                                                params = list(
+                                                    id = list(inputId = "id", title = "Survey:", placeholder = 'select'),
+                                                    transect = list(inputId = "transect", title = "Transect:", placeholder = 'select'),
+                                                    date = list(inputId = "date", title = "Date:", placeholder = 'select'))
+                                            )
+                              )
+                          )
+                 )
+)
+
+
+#-------------------------------------------------------------------------------
+
+# Define server logic
+server <- function(input, output, session) {
+
+    options(shiny.maxRequestSize=30*1024^2)
+
+    #----
+    ## Tab 1: Load data
+
+    # Define the uploaded .Rdata file
+    dat <- reactive({
+        req(input$file)
+        if (is.null(input$file))
+            return(NULL)
+        inFile <- input$file
+        file <- inFile$datapath
+        # Load the file into new environment and get it from there
+        e = new.env()
+        name <- load(file, envir = e)
+        dat <- e[[name]]
+        dat <- dat %>%
+            filter(!is.na(lat))  # Filter out rows that are missing latitude values
+        dat
+    })
+
+    # Summarize data for the summary table
+    dat.tbl <- reactive({
+        req(input$file)
+        dat() %>%
+            group_by("Transect ID" = id,
+                     "Source file" = sourcefile) %>%
+            summarise(
+                "Transect" = transect,
+                "Survey date" = date)
+    })
+
+    # Create a DT table of the summarized data
+    output$sum.tbl <- DT::renderDT(dat.tbl(), options = list(pageLength = 20))
+
+
+    #----
+    ## Tab 2: Map
+
+    # Subset the data based on user input from selectizeGroupUI:
+    dat.sub <- callModule(
+        id = "my-filters",
+        module = selectizeGroupServer,
+        data = dat,
+        vars = c("id","transect","date")
+    )
+
+    # Create a map of the subsetted data:
+    output$map <- renderLeaflet({
+        nmbms_map(dat.sub())
+    })
+
+    # Output info for fixes selected in the map
+    output$dataInfo <- renderUI({
+        HTML(
+            paste(sep = "<br/>",
+                  paste("<br>"),
+                  paste("<b>Total Surveys:</b> ", length(unique(dat.sub()$id))),
+                  paste("<b>Total Transects:</b> ", length(unique(dat.sub()$transect))),
+                  paste("<b>Min. Date:</b> ", as.Date(min(dat.sub()$datetime))),
+                  paste("<b>Max. Date:</b> ", as.Date(max(dat.sub()$datetime))),
+                  paste("<br>")
+            ))
+    })
+
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
---
+++ code/global.R
@@ -0,0 +1,89 @@
+################################################################################
+# Global functions used in NMBMS transect-viewer shiny app
+# Jared Laufenberg <jared_laufenberg@fws.gov>
+################################################################################
+
+
+
+#----
+## Aesthetics
+
+# Color palatte used in the application
+color_pal <- c("#3366CC", "#DC3912", "#FF9900", "#109618", "#990099", "#0099C6",
+               "#DD4477", "#66AA00", "#B82E2E", "#316395", "#994499", "#22AA99",
+               "#AAAA11", "#6633CC", "#E67300", "#8B0707", "#651067", "#329262",
+               "#5574A6", "#3B3EAC")
+
+
+
+#----
+## Mapping functions
+
+
+#' Leaflet map of Kodiak NWR NMBMS data
+#'
+#' @description Map distance sampling transect data. First and last
+#' points.
+#'
+#' @author Jared Laufenberg <jared_laufenberg@@fws.gov>
+#'
+#' @param nmbms A dataframe of GPS collar fixes containing lat, lon, and animal_id, site and fix_time fields
+#'
+#' @return A leaflet map of fix locations, subsetted to one daily fix.
+#' @export
+#'
+#' @examples collar_map(dat)
+#'
+nmbms_map <- function(nmbms) {
+  df <- nmbms %>%
+    filter(!(is.na(lon) | is.na(lat))) %>%
+    arrange(id, datetime) #%>%
+##    group_by(id, lubridate::as_date(datetime)) %>%
+##    slice(1) %>%  # takes the first fix of the day for each collar
+##    ungroup()
+
+  ids <- unique(df$id)
+  pal <- rep_len(color_pal, length(ids))
+
+  map <- leaflet() %>%
+    addProviderTiles("Esri.WorldTopoMap", options = providerTileOptions(attribution = NA)) %>%
+    addMeasure(primaryLengthUnit="kilometers", secondaryLengthUnit="kilometers") %>%
+    addScaleBar(position="bottomleft") %>%
+    setView(lng = -153, lat = 57.5, zoom = 9)
+
+
+  for (i in seq_along(ids)) {
+    d <- df %>% filter(id == ids[i])
+    dp <- d[c(1, nrow(d)), ]
+    dp$endpoints <- c("START","END")
+    map <- addPolylines(map, lng = d$lon, lat = d$lat,
+                        weight = 1,
+                        color = pal[i],
+                        opacity = .4)
+    map <- addCircleMarkers(map, lng = dp$lon, lat = dp$lat,
+                            stroke = FALSE,
+                            radius = 4,
+                            color = c("green","red"),
+                            fillOpacity = 1,
+                            popup = paste(sep = "<br>",
+                                          paste("<b>Position:<b>", dp$endpoints),
+                                          paste("<b>Transect ID:<b>", dp$transect),
+                                          paste("<b>Survey date:<b>", dp$date))
+                            )
+    o <- d %>% filter(spp == "MAMU")
+    map <- addCircleMarkers(map, lng = o$lon, lat = o$lat,
+                            radius = o$count,
+                            color = "grey",
+                            fillOpacity = 1,
+                            popup = paste(sep = "<br>",
+                                          paste("<b>Count:<b>", o$count),
+                                          paste("<b>Distance:<b>", o$distance))
+    )
+  }
+  return(map)
+}
+
+
+
+
+
